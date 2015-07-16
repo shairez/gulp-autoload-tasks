eab6e2194837b2d094aa255330a1fb2f47a4757b
shairez <psyrez@gmail.com>
2015-07-16 18:54:59 +0300

    fixed directory root issue

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var wrench = require('wrench');
+var path = require('path');
 
 module.exports = function(gulp, gulpPlugins, config, gulpDir){
 
-  gulpDir = gulpDir || './gulp_tasks';
+  gulpDir = gulpDir || 'gulp_tasks';
+
+  if (gulpDir[0] !== path.sep && gulpDir.slice(0, 2) !== '.' + path.sep) {
+    gulpDir = path.join(process.cwd(), gulpDir);
+  }
 
   wrench.readdirSyncRecursive(gulpDir)
     .filter(function(file){
